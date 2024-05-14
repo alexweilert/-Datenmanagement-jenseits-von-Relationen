4716466869a299ef68d4370b44fb80070d819000
Alexander Weilert <s1085558@stud.sbg.ac.at>
2024-05-14 23:23:42 +0200

    Ansatz2 erledigt i guess.

--- src/db/Effiziente_Matrixmultiplikation.java
+++ src/db/Effiziente_Matrixmultiplikation.java
@@ -37,8 +37,10 @@ public class Effiziente_Matrixmultiplikation {
             int[][][] matrix = new int[2][][];
             int[][] matrixA = new int[l-1][l];
             int[][] matrixB = new int[l][l-1];
-
+            
+            //System.out.println("--- Matrix A ---");
             matrix[0] = generateMatrix(matrixA, sparsity);
+            //System.out.println("--- Matrix B ---");
             matrix[1] = generateMatrix(matrixB, sparsity);
 
             insertMatrix("A", matrix[0]);
@@ -52,7 +54,6 @@ public class Effiziente_Matrixmultiplikation {
 
     public int[][] generateMatrix(int[][] matrix, double sparsity) {
         Random random = new Random();
-        System.out.println("--- Matrix A ---");
         for (int i = 0; i < matrix.length; i++) {
             for (int j = 0; j < matrix[0].length; j++) {
                 if (random.nextDouble() > sparsity) {
@@ -60,9 +61,9 @@ public class Effiziente_Matrixmultiplikation {
                 } else {
                     matrix[i][j] = 0;
                 }
-                System.out.print(matrix[i][j] + " ");
+                //System.out.print(matrix[i][j] + " ");
             }
-            System.out.println();
+            //System.out.println();
         }
         return matrix;
     }
@@ -92,16 +93,16 @@ public class Effiziente_Matrixmultiplikation {
             statement.execute("CREATE TABLE matrix_algorithm (i INT, j INT, val INT, PRIMARY KEY (i, j))");
             StringBuilder insertQuery = new StringBuilder("INSERT INTO matrix_algorithm VALUES ");
             int[][] result = new int[matrixA.length][matrixB[0].length];
-            System.out.println("--- Matrix Calculator ---");
+            //System.out.println("--- Matrix Calculator ---");
             for (int i = 0; i < matrixA.length; i++) {
                 for (int j = 0; j < matrixB[0].length; j++) {
                     for (int k = 0; k < matrixA[0].length; k++) {
                         result[i][j] += matrixA[i][k] * matrixB[k][j];
                     }
                     insertQuery.append("(").append(i + 1).append(",").append(j + 1).append(",").append(result[i][j]).append("),");
-                    System.out.print(result[i][j] + " ");
+                    //System.out.print(result[i][j] + " ");
                 }
-                System.out.println();
+                //System.out.println();
             }
             insertQuery.deleteCharAt(insertQuery.length() - 1);
             statement.executeUpdate(insertQuery.toString());
@@ -123,23 +124,21 @@ public class Effiziente_Matrixmultiplikation {
         }
     }
 
-    public void ansatz2(int[][][] matrix) {
+    public void createVectorTable(int[][][] matrix) {
         try (Statement statement = this.connection.createStatement()) {
-            int[][] matrixA = matrix[0];
-            int[][] matrixB = matrix[1];
             statement.execute("DROP TABLE IF EXISTS new_A, new_B");
             statement.execute("CREATE TABLE new_A (i INT, row INT[], PRIMARY KEY (i))");
             statement.execute("CREATE TABLE new_B (j INT, col INT[], PRIMARY KEY (j))");
 
-            insertAnsatz("new_A", matrix[0]);
-            insertAnsatz("new_B", matrix[1]);
+            insertVector("new_A", matrix[0]);
+            insertVector("new_B", matrix[1]);
 
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
     }
 
-    public void insertAnsatz(String table_name, int[][] matrix){
+    public void insertVector(String table_name, int[][] matrix){
         try (Statement statement = this.connection.createStatement()) {
             for (int i = 0; i < matrix.length; i++) {
                 StringBuilder updateQueryA = new StringBuilder("INSERT INTO " + table_name + " VALUES (");
@@ -153,6 +152,45 @@ public class Effiziente_Matrixmultiplikation {
                 updateQueryA.append("])");
                 statement.executeUpdate(updateQueryA.toString());
             }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void ansatz2() {
+        try (Statement statement = this.connection.createStatement()) {
+            statement.execute("DROP TABLE IF EXISTS ansatz2");
+            statement.execute("CREATE TABLE ansatz2 (i INT, j INT, val INT, PRIMARY KEY (i, j))");
+
+            createFunction();
+
+            statement.execute("INSERT INTO ansatz2 " +
+                    "SELECT new_A.i, new_B.j, dotproduct(new_A.row, new_B.col) " +
+                    "FROM new_A, new_B " +
+                    "WHERE new_A.i = new_B.j " +
+                    "GROUP BY new_a.i, new_b.j");
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void createFunction() {
+        try (Statement statement = this.connection.createStatement()) {
+            statement.execute(
+                "CREATE OR REPLACE FUNCTION dotproduct(int[], int[]) RETURNS int AS $$\n" +
+                        "DECLARE\n" +
+                        "    result int := 0;\n" +
+                        "    i int;\n" +
+                        "    len int;\n" +
+                        "BEGIN\n" +
+                        "    len := LEAST(array_length($1, 1), array_length($2, 1));\n" +
+                        "    FOR i IN 1 .. len LOOP\n" +
+                        "            result := result + ($1[i] * $2[i]);\n" +
+                        "        END LOOP;\n" +
+                        "RETURN result;\n" +
+                        "END;\n" +
+                        "$$ LANGUAGE plpgsql;\n");
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
--- src/db/Effiziente_Matrixmultiplikation_Main.java
+++ src/db/Effiziente_Matrixmultiplikation_Main.java
@@ -12,9 +12,42 @@ public class Effiziente_Matrixmultiplikation_Main {
         matrix = e_matrix_mult.generate(5, 0.5);
         e_matrix_mult.ansatz0(matrix);
         e_matrix_mult.ansatz1();
-        e_matrix_mult.ansatz2(matrix);
+        e_matrix_mult.createVectorTable(matrix);
+        e_matrix_mult.ansatz2();
+
+        //benchmark(e_matrix_mult);
 
         e_matrix_mult.closeConnection();
     }
-    
+
+    public static void benchmark(Effiziente_Matrixmultiplikation e_matrix_mult) throws SQLException, ClassNotFoundException {
+        int[][][] matrix;
+        System.out.println("Benchmark enrolling");
+        long startTime = System.currentTimeMillis();
+        System.out.println("Test 1: Extending size of Matrix:");
+        for(int i = 3; i < 13; i++) {
+            System.out.println("Testing with: L: " + i + ", Sparsity: 0.5");
+            matrix = e_matrix_mult.generate((int) Math.pow(2, i), 0.5);
+            System.out.println("Matrix generated in " + (System.currentTimeMillis() - startTime) + " ms");
+            benchmark_calculation(e_matrix_mult, matrix, startTime);
+        }
+
+        System.out.println("Test 2: Extending Sparsity of Matrix:");
+        for(double i = 0.1; i < 0.99; i += 0.1) {
+            System.out.println("----- ----- -----");
+            System.out.println("L: 10, Sparsity: " + i);
+            matrix = e_matrix_mult.generate((int) Math.pow(2, 3), i);
+            System.out.println("Matrix in " + (System.currentTimeMillis() - startTime) + " ms");
+            benchmark_calculation(e_matrix_mult, matrix, startTime);
+        }
+    }
+
+    public static void benchmark_calculation(Effiziente_Matrixmultiplikation e_matrix_mult, int[][][] matrix, long startTime) {
+        e_matrix_mult.ansatz0(matrix);
+        System.out.println("A0 Finished in " + (System.currentTimeMillis() - startTime) + " ms");
+        e_matrix_mult.ansatz1();
+        System.out.println("A1 Finished in " + (System.currentTimeMillis() - startTime) + " ms");
+        e_matrix_mult.createVectorTable(matrix);
+        System.out.println("A2 Finished in " + (System.currentTimeMillis() - startTime) + " ms");
+    }
 }
